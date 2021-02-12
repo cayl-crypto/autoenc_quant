371ac39d1edd178d69eeec6a5c9e39f89cd3df44
cayl-crypto <caylio64@gmail.com>
2021-02-12 20:45:17 +0300

    Autoencoder works should be trained.

--- autoencoder.py
+++ autoencoder.py
@@ -8,41 +8,43 @@ from torch.utils.data.sampler import SubsetRandomSampler
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
-# Converting data to torch.FloatTensor
-transform = transforms.ToTensor()
 
-# Download the training and test datasets
-train_data = datasets.CIFAR10(root='data', train=True, download=True, transform=transform)
-
-test_data = datasets.CIFAR10(root='data', train=False, download=True, transform=transform)
-
-# Prepare data loaders
-train_loader = torch.utils.data.DataLoader(train_data, batch_size=32, num_workers=0)
-test_loader = torch.utils.data.DataLoader(test_data, batch_size=32, num_workers=0)
-
-
-# Utility functions to un-normalize and display an image
-def imshow(img):
-    img = img / 2 + 0.5
-    plt.imshow(np.transpose(img, (1, 2, 0)))
-
-
-# Define the image classes
-classes = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
-
-# Obtain one batch of training images
-dataiter = iter(train_loader)
-images, labels = dataiter.next()
-images = images.numpy()  # convert images to numpy for display
-
-# Plot the images
-fig = plt.figure(figsize=(8, 8))
-# display 20 images
-for idx in np.arange(9):
-    ax = fig.add_subplot(3, 3, idx + 1, xticks=[], yticks=[])
-    imshow(images[idx])
-    ax.set_title(classes[labels[idx]])
-
+#
+# # Converting data to torch.FloatTensor
+# transform = transforms.ToTensor()
+#
+# # Download the training and test datasets
+# train_data = datasets.CIFAR10(root='data', train=True, download=True, transform=transform)
+#
+# test_data = datasets.CIFAR10(root='data', train=False, download=True, transform=transform)
+#
+# # Prepare data loaders
+# train_loader = torch.utils.data.DataLoader(train_data, batch_size=32, num_workers=0)
+# test_loader = torch.utils.data.DataLoader(test_data, batch_size=32, num_workers=0)
+#
+#
+# # Utility functions to un-normalize and display an image
+# def imshow(img):
+#     img = img / 2 + 0.5
+#     plt.imshow(np.transpose(img, (1, 2, 0)))
+#
+#
+# # Define the image classes
+# classes = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+#
+# # Obtain one batch of training images
+# dataiter = iter(train_loader)
+# images, labels = dataiter.next()
+# images = images.numpy()  # convert images to numpy for display
+#
+# # Plot the images
+# fig = plt.figure(figsize=(8, 8))
+# # display 20 images
+# for idx in np.arange(9):
+#     ax = fig.add_subplot(3, 3, idx + 1, xticks=[], yticks=[])
+#     imshow(images[idx])
+#     ax.set_title(classes[labels[idx]])
+#
 
 # Define the Convolutional Autoencoder
 class ConvAutoencoder(nn.Module):
@@ -55,92 +57,115 @@ class ConvAutoencoder(nn.Module):
         self.pool = nn.MaxPool2d(2, 2)
 
         # Decoder
-        self.t_conv1 = nn.ConvTranspose2d(4, 16, 2, stride=2)
+        self.t_conv1 = nn.ConvTranspose2d(4, 16, 3, stride=2)
         self.t_conv2 = nn.ConvTranspose2d(16, 3, 2, stride=2)
+        self.flat = nn.Flatten()
+        # self.up = nn.Upsample(scale_factor=2, mode='nearest')
+
 
     def forward(self, x):
+        input_size = x.size()
+        # print(input_size)
         x = F.relu(self.conv1(x))
+        # print(x.size())
         x = self.pool(x)
+        # print(x.size())
         x = F.relu(self.conv2(x))
+        # print(x.size())
         x = self.pool(x)
+        # print(x.size())
         x = F.relu(self.t_conv1(x))
-        x = F.sigmoid(self.t_conv2(x))
+        # print(x.size())
+        x = torch.sigmoid(self.t_conv2(x, output_size=input_size))
+        # print(x.size())
 
         return x
 
-
-# Instantiate the model
-model = ConvAutoencoder()
-print(model)
-
-# Loss function
-criterion = nn.BCELoss()
-
-# Optimizer
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-
-
-def get_device():
-    if torch.cuda.is_available():
-        device = 'cuda:0'
-    else:
-        device = 'cpu'
-    return device
-
-
-device = get_device()
-print(device)
-
-model.to(device)
-# Epochs
-n_epochs = 100
-
-for epoch in range(1, n_epochs + 1):
-    # monitor training loss
-    train_loss = 0.0
-
-    # Training
-    for data in train_loader:
-        images, _ = data
-        images = images.to(device)
-        optimizer.zero_grad()
-        outputs = model(images)
-        loss = criterion(outputs, images)
-        loss.backward()
-        optimizer.step()
-        train_loss += loss.item() * images.size(0)
-
-    train_loss = train_loss / len(train_loader)
-    print('Epoch: {} \tTraining Loss: {:.6f}'.format(epoch, train_loss))
-
-#Batch of test images
-dataiter = iter(test_loader)
-images, labels = dataiter.next()
-images = images.to(device)
-
-#Sample outputs
-output = model(images)
-images = images.cpu()
-batch_size = 32
-output = output.view(batch_size, 3, 32, 32)
-output = output.detach().cpu()
-
-#Original Images
-print("Original Images")
-fig, axes = plt.subplots(nrows=1, ncols=5, sharex=True, sharey=True, figsize=(12,4))
-for idx in np.arange(5):
-    ax = fig.add_subplot(1, 5, idx+1, xticks=[], yticks=[])
-    imshow(images[idx])
-    ax.set_title(classes[labels[idx]])
-plt.show()
-
-#Reconstructed Images
-print('Reconstructed Images')
-fig, axes = plt.subplots(nrows=1, ncols=5, sharex=True, sharey=True, figsize=(12,4))
-for idx in np.arange(5):
-    ax = fig.add_subplot(1, 5, idx+1, xticks=[], yticks=[])
-    imshow(output[idx])
-    ax.set_title(classes[labels[idx]])
-plt.show()
-
-
+    def encode(self, x):
+        x = F.relu(self.conv1(x))
+        # print(x.size())
+        x = self.pool(x)
+        # print(x.size())
+        x = F.relu(self.conv2(x))
+        # print(x.size())
+        x = self.pool(x)
+        x = self.pool(x)
+        x = self.flat(x)
+        return x
+#
+#
+# # Instantiate the model
+# model = ConvAutoencoder()
+# print(model)
+#
+# # Loss function
+# criterion = nn.BCELoss()
+#
+# # Optimizer
+# optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+#
+#
+# def get_device():
+#     if torch.cuda.is_available():
+#         device = 'cuda:0'
+#     else:
+#         device = 'cpu'
+#     return device
+#
+#
+# device = get_device()
+# print(device)
+#
+# model.to(device)
+# # Epochs
+# n_epochs = 100
+#
+# for epoch in range(1, n_epochs + 1):
+#     # monitor training loss
+#     train_loss = 0.0
+#
+#     # Training
+#     for data in train_loader:
+#         images, _ = data
+#         images = images.to(device)
+#         optimizer.zero_grad()
+#         outputs = model(images)
+#         loss = criterion(outputs, images)
+#         loss.backward()
+#         optimizer.step()
+#         train_loss += loss.item() * images.size(0)
+#
+#     train_loss = train_loss / len(train_loader)
+#     print('Epoch: {} \tTraining Loss: {:.6f}'.format(epoch, train_loss))
+#
+# #Batch of test images
+# dataiter = iter(test_loader)
+# images, labels = dataiter.next()
+# images = images.to(device)
+#
+# #Sample outputs
+# output = model(images)
+# images = images.cpu()
+# batch_size = 32
+# output = output.view(batch_size, 3, 32, 32)
+# output = output.detach().cpu()
+#
+# #Original Images
+# print("Original Images")
+# fig, axes = plt.subplots(nrows=1, ncols=5, sharex=True, sharey=True, figsize=(12,4))
+# for idx in np.arange(5):
+#     ax = fig.add_subplot(1, 5, idx+1, xticks=[], yticks=[])
+#     imshow(images[idx])
+#     ax.set_title(classes[labels[idx]])
+# plt.show()
+#
+# #Reconstructed Images
+# print('Reconstructed Images')
+# fig, axes = plt.subplots(nrows=1, ncols=5, sharex=True, sharey=True, figsize=(12,4))
+# for idx in np.arange(5):
+#     ax = fig.add_subplot(1, 5, idx+1, xticks=[], yticks=[])
+#     imshow(output[idx])
+#     ax.set_title(classes[labels[idx]])
+# plt.show()
+#
+#
--- training.py
+++ training.py
@@ -5,6 +5,7 @@ import csv
 import torch
 import torchvision
 import torchvision.transforms as transforms
+from autoencoder import ConvAutoencoder
 
 torch.manual_seed(12321)
 
@@ -59,38 +60,16 @@ def read_csv2labels(path):
     return labeli
 
 
-data_folder = "Dataset/"
-labels_train = read_csv2labels(data_folder + 'shuffled_quant_dataset_labels_train.csv')
-labels_test = read_csv2labels(data_folder + 'quant_dataset_labels_test.csv')
 # for a in labels:
 #     print(a[0], a[1])
 # print()
 
 # MODEL
 import torch.nn as nn
-import torch.nn.functional as F
 
-
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 34 * 34, 120)
-        self.fc2 = nn.Linear(120, 64)
-        self.fc3 = nn.Linear(64, 6)
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 34 * 34)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-
-
+data_folder = "Dataset/"
+labels_train = read_csv2labels(data_folder + 'shuffled_quant_dataset_labels_train.csv')
+labels_test = read_csv2labels(data_folder + 'shuffled_quant_dataset_labels_test.csv')
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 print(device)
@@ -98,15 +77,20 @@ print(device)
 import torch.optim as optim
 
 # SHUFFLE DATASET
-from sklearn.utils import shuffle
-
+# from sklearn.utils import shuffle
+#
 # labels_train = shuffle(labels_train)
-# f = open('shuffled_quant_dataset_labels_train.csv', 'w')
+# f = open(data_folder + 'shuffled_quant_dataset_labels_train.csv', 'w')
 #
 # with f:
 #     writer = csv.writer(f)
 #     writer.writerows(labels_train)
 # labels_test = shuffle(labels_test)
+# f = open(data_folder + 'shuffled_quant_dataset_labels_test.csv', 'w')
+#
+# with f:
+#     writer = csv.writer(f)
+#     writer.writerows(labels_test)
 
 # TRAIN
 
@@ -114,68 +98,86 @@ import time as t
 
 
 def train(net, root):
-    result_list = [["Dataset", "Epoch", "Accuracy", "Time", "Total Correct", "Total Test Sample"]]
+    max_test_loss = 9999.9
+    result_list = [["Dataset", "Epoch", "Time", "Total Train Loss", "Total Test Loss"]]
     print(root)
     net.to(device)
     net.cuda()
     batch_size = 64
-    image_batch = torch.zeros(batch_size, 3, 150, 150)
-    label_batch = torch.zeros(batch_size)
-    label_batch = label_batch.type(torch.LongTensor)
-    image_batch = image_batch.cuda()
-    label_batch = label_batch.cuda()
-    image_batch.to(device)
-    label_batch.to(device)
+    image_batch_i = torch.zeros(batch_size, 3, 150, 150)
+    image_batch_i = image_batch_i.cuda()
+    image_batch_i.to(device)
 
-    criterion = nn.CrossEntropyLoss()
+    image_batch_o = torch.zeros(batch_size, 3, 150, 150)
+    image_batch_o = image_batch_o.cuda()
+    image_batch_o.to(device)
+
+    image_batch_t_i = torch.zeros(batch_size, 3, 150, 150)
+    image_batch_t_i = image_batch_t_i.cuda()
+    image_batch_t_i.to(device)
+
+    image_batch_t_o = torch.zeros(batch_size, 3, 150, 150)
+    image_batch_t_o = image_batch_t_o.cuda()
+    image_batch_t_o.to(device)
+
+    criterion = nn.BCELoss()
+
     optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-    preprocess = transforms.Compose([
+    preprocess_i = transforms.Compose([
         transforms.Resize(150),
         transforms.CenterCrop(150),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
-    for epoch in tqdm(range(100)):
+
+    preprocess_o = transforms.Compose([
+        transforms.Resize(150),
+        transforms.CenterCrop(150),
+        transforms.ToTensor(),
+    ])
+
+    for epoch in tqdm(range(1)):
+        # i stands for input, o stands for output
         start_time = t.time()
         # Extract all features with batches
         net.train()
-        running_loss = 0.0
+        total_train_loss = 0.0
         for ids, data in enumerate(labels_train):
             # completes in about .
-            im_path = data_folder + root + '/' + data[0]
-            label = data[1]
-            im = load_image(im_path)
-            im = gray_to_rgb(im)
-            im = preprocess(im)
+            img_path = data_folder + root + '/' + data[0]
+            img = load_image(img_path)
+            img = gray_to_rgb(img)
+            im_i = preprocess_i(img)
+            im_o = preprocess_o(img)
             batch_index = ids % batch_size
-            image_batch[batch_index] = im
-            label_batch[batch_index] = int(label)
+            image_batch_i[batch_index] = im_i
+            image_batch_o[batch_index] = im_o
 
             if ids == len(labels_train) - 1:
-                image_batch_last = image_batch[:batch_index + 1]
-                label_batch_last = label_batch[:batch_index + 1]
+                image_batch_last_i = image_batch_i[:batch_index + 1]
+                image_batch_last_o = image_batch_o[:batch_index + 1]
                 optimizer.zero_grad()
-                outputs = net(image_batch_last)
-                loss = criterion(outputs, label_batch_last)
+                results = net(image_batch_last_i)
+                loss = criterion(results, image_batch_last_o)
                 loss.backward()
                 optimizer.step()
-                running_loss += loss.item()
+                total_train_loss += loss.item()
                 print('loss: %f' %
-                      (running_loss / 14034))
-                running_loss = 0.0
-                # break
+                      (total_train_loss / 14034))
 
             if batch_index == batch_size - 1:
                 optimizer.zero_grad()
-                outputs = net(image_batch)
-                loss = criterion(outputs, label_batch)
+                results = net(image_batch_i)
+                # encoded features are x
+                # x = net.encode(image_batch_i)
+                ########################
+                loss = criterion(results, image_batch_o)
                 loss.backward()
                 optimizer.step()
-                running_loss += loss.item()
+                total_train_loss += loss.item()
 
         net.eval()
-        total = 0
-        correct = 0
+        total_test_loss = 0.0
         with torch.no_grad():
             for ids, data in enumerate(labels_test):
                 # completes in about .
@@ -183,29 +185,31 @@ def train(net, root):
                 label = data[1]
                 im = load_image(im_path)
                 im = gray_to_rgb(im)
-                im = preprocess(im)
+                im_i = preprocess_i(im)
+                im_o = preprocess_o(im)
                 batch_index = ids % batch_size
-                image_batch[batch_index] = im
-                label_batch[batch_index] = int(label)
+                image_batch_t_i[batch_index] = im_i
+                image_batch_t_o[batch_index] = im_o
 
                 if ids == len(labels_test) - 1:
-                    image_batch_last = image_batch[:batch_index + 1]
-                    label_batch_last = label_batch[:batch_index + 1]
-                    outputs = net(image_batch_last)
-                    _, predicted = torch.max(outputs, 1)
-                    total += label_batch_last.size(0)
-                    res = sum(predicted == label_batch_last)
-                    correct += res.item()
+                    image_batch_last_t_i = image_batch_t_i[:batch_index + 1]
+                    image_batch_last_t_o = image_batch_t_o[:batch_index + 1]
+                    outputs = net(image_batch_last_t_i)
+                    loss = criterion(outputs, image_batch_last_t_o)
+                    total_test_loss += loss.item()
+                    if total_test_loss < max_test_loss:
+                        # save best model
+                        torch.save(net, "best_" + root + ".pt")
+                        max_test_loss = total_test_loss
                     break
 
                 if batch_index == batch_size - 1:
-                    outputs = net(image_batch)
-                    _, predicted = torch.max(outputs, 1)
-                    total += label_batch.size(0)
-                    res = sum(predicted == label_batch)
-                    correct += res.item()
+                    outputs = net(image_batch_t_i)
+                    loss = criterion(outputs, image_batch_t_o)
+                    total_test_loss += loss.item()
 
-            result_list.append([root, epoch + 1, correct / total, t.time() - start_time, correct, total])
+            result_list.append(
+                [root, epoch + 1, t.time() - start_time, str(total_train_loss / 14034), str(total_test_loss / 3000)])
 
     f = open(root + '_results.csv', 'w')
 
@@ -219,31 +223,57 @@ def train(net, root):
 total_time = t.time()
 
 # Train Original
-# original_net = Net()
-# result_list = train(original_net, roots[0])
-# # Train 2
-# _2_net = Net()
-# result_list = train(_2_net, roots[1])
-# # # Train 4
-# _4_net = Net()
-# result_list = train(_4_net, roots[2])
-# # # Train 8
-# _8_net = Net()
-# result_list = train(_8_net, roots[3])
-# # # Train 16
-# _16_net = Net()
-# result_list = train(_16_net, roots[4])
-# # # Train 32
-# _32_net = Net()
-# result_list = train(_32_net, roots[5])
-# # # Train 64
-# _64_net = Net()
-# result_list = train(_64_net, roots[6])
-# # # Train 128
-# _128_net = Net()
-# result_list = train(_128_net, roots[7])
-# # # Train 256
-# _256_net = Net()
-# result_list = train(_256_net, roots[8])
+original_net = ConvAutoencoder()
+print(original_net)
+original_net = train(original_net, roots[0])
+
+# Train 2
+_2_net = ConvAutoencoder()
+result_list = train(_2_net, roots[1])
+# # Train 4
+_4_net = ConvAutoencoder()
+result_list = train(_4_net, roots[2])
+# # Train 8
+_8_net = ConvAutoencoder()
+result_list = train(_8_net, roots[3])
+# # Train 16
+_16_net = ConvAutoencoder()
+result_list = train(_16_net, roots[4])
+# # Train 32
+_32_net = ConvAutoencoder()
+result_list = train(_32_net, roots[5])
+# # Train 64
+_64_net = ConvAutoencoder()
+result_list = train(_64_net, roots[6])
+# # Train 128
+_128_net = ConvAutoencoder()
+result_list = train(_128_net, roots[7])
+# # Train 256
+_256_net = ConvAutoencoder()
+result_list = train(_256_net, roots[8])
 print(t.time() - total_time)
 print('Finished Training')
+
+# preprocess = transforms.Compose([
+#     transforms.Resize(150),
+#     transforms.CenterCrop(150),
+#     transforms.ToTensor(),
+#     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+# ])
+#
+# im_path = "20057.jpg"
+# im = load_image(im_path)
+# im = gray_to_rgb(im)
+# im = preprocess(im)
+# im = torch.unsqueeze(im, 0)
+#
+# im = im.cuda()
+# im.to(device)
+# original_net.eval()
+# with torch.no_grad():
+#     output = original_net(im)
+#     output = torch.squeeze(output, 0)
+#     s = transforms.ToPILImage()
+#     # show_image(output.permute(1, 2, 0).numpy())
+#     print(output.shape)
+#     show_image(s(output))
